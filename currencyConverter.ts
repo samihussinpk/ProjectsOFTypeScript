c9ea5a85b5ffc02e7cbc4e5bfe243bd3d80fa274
Sami Hussain <161669469+samihussinpk@users.noreply.github.com>
2024-07-16 03:29:22 -0700

    Add files via upload

---
+++ currencyConverter.ts
@@ -0,0 +1,49 @@
+import inquirer from "inquirer";
+import chalk from "chalk"
+console.log(chalk.blue.bold("Welcome To Currency Conversion Project\n"));
+
+
+let exchangeRates: any = {
+    "USD" : 1, //Base currency
+    "EUR" : 0.9, // european currency (Euro)
+    "JYP" : 113, // Japenese Currecny (Yen) 
+    "CAD" : 1.3,  //Canadian Dollar
+    "AUD" : 1.65,  //Australian doller
+    "PKR" :278.56 //Pakistani Rupees
+
+}
+
+let userAnswer = await inquirer.prompt([
+    {
+        name:"from_currency",
+        message:"Select the currency to convert",
+        type:"list",
+        choices:["USD","EUR","JYP","CAD","AUD","PKR"]
+    },
+    {
+        name:"to_currency",
+        message:"Select the currency in which you want to convert.",
+        type:"list",
+        choices:["USD","EUR","JYP","CAD","AUD","PKR"]
+
+    },
+    {
+        name:"amount",
+        message:"Enter your amount to convert",
+        type:"input"
+    }
+])
+
+// Currency conversion by using formula
+let fromAmount = exchangeRates[userAnswer.from_currency];
+let toAmount = exchangeRates[userAnswer.to_currency]
+let userAmount = userAnswer.amount 
+
+// formulae for conversion
+let baseAmount = userAmount /fromAmount
+let convertedAmount = baseAmount *toAmount
+
+// Converted amount will be displayed here and ".toFixed" is used to round off the amount 
+console.log(`The converted amount is ${convertedAmount.toFixed(3)}`);
+
+
